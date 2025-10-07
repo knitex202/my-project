c9228e8968e8bcf42efb35a58d67e803cd3ff822
knitex202 <knitex303@yahoo.com>
2025-10-07 11:14:52 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  base: '/my-project/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),}
+  }
 })
